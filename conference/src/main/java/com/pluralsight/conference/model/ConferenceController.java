b7c16fef90d2c7f6f7a36775cbf388751663e10a
Adam Wrutniak <adam.wrutniak@capgemini.com>
2023-05-01 13:57:25 +0200

    V0.2: Adding Thymeleaf dependency TemplateResovler

--- conference/src/main/java/com/pluralsight/conference/model/ConferenceController.java
+++ conference/src/main/java/com/pluralsight/conference/model/ConferenceController.java
@@ -1,8 +1,8 @@
 package com.pluralsight.conference.model;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,13 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import java.util.Locale;
 
 @Configuration
 public class ConferenceController implements WebMvcConfigurer {
 
-
+    private ApplicationContext applicationContext;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
@@ -51,4 +51,14 @@ public class ConferenceController implements WebMvcConfigurer {
         bean.setOrder(0);
         return bean;
     }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
 }
